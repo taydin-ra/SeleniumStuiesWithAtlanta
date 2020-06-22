3a8365d76e652554c5bfaefb16ddb9ed86d9fda1
taydin-ra <ttorun16@gmail.com>
2020-06-22 16:17:23 -0400

    pushed

---
+++ src/study/techno/Main.java
@@ -0,0 +1,14 @@
+package study.techno;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utility.BaseClass;
+
+public class Main  extends BaseClass {
+
+    public static void main(String[] args) {
+
+        driver.get("https://formsmarts.com/form/yu?mode=h5");
+
+    }
+}
---
+++ src/utility/BaseClass.java
@@ -0,0 +1,14 @@
+package utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+
+    public static WebDriver driver;
+
+    static {
+        System.setProperty("webdriver.chrome.driver", "/Users/yavuzaydin/Desktop/selenium/chromedriver 2");
+        driver = new ChromeDriver();
+    }
+}
