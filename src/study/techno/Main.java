b2105953e63299a03eae888850e1d897d7d9d542
taydin-ra <ttorun16@gmail.com>
2020-06-22 17:30:43 -0400

    pushed 270876990

--- src/study/techno/Main.java
+++ src/study/techno/Main.java
@@ -1,6 +1,8 @@
 package study.techno;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utility.BaseClass;
 
@@ -9,6 +11,18 @@ public class Main  extends BaseClass {
     public static void main(String[] args) {
 
         driver.get("https://www.etsy.com/");
+
+        WebElement element = driver.findElement(By.cssSelector("input[data-id='search-query']")); // xpathden daha hizli calisir
+        element.sendKeys("Garden");
+
+        WebElement searchButton=driver.findElement(By.cssSelector("button[aria-label='Search']"));
+        searchButton.click();
+
+        // cocuk icin >              div[id='section_1'] legend[id='u_SCX_4586_lbl']+label>span
+        // torun icin bosluk         div[id='section_1'] legend[id='u_SCX_4586_lbl']
+        // ardisik kardes icin +     div[id='section_1'] legend[id='u_SCX_4586_lbl']+label
+
+
 
     }
 }
