2ad683eb2c4346e5e671e0f87fc60218da13a3a7
taydin-ra <ttorun16@gmail.com>
2020-07-08 17:05:01 -0400

    waits are added

--- src/study/techno/implicitAndExplicitWait/Demo.java
+++ src/study/techno/implicitAndExplicitWait/Demo.java
@@ -1,6 +1,7 @@
 package study.techno.implicitAndExplicitWait;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,6 +12,16 @@ import java.util.List;
 public class Demo extends BaseClass {
     public static void main(String[] args) throws InterruptedException {
         driver.manage().window().maximize();
-        driver.get(" http://demo.automationtesting.in/WebTable.html");
+        driver.get(" http://demo.automationtesting.in/WebTable.html"); //
+        WebDriverWait wait = new WebDriverWait(driver, 6);
+        WebElement table = driver.findElement(By.xpath("//div[@class='ui-grid-canvas']"));
+
+        List<WebElement> emails = driver.findElements(By.xpath("//div[@class='ui-grid-canvas']/div/div"));
+        // wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//div[@class='ui-grid-canvas']/div/div"),5));
+        wait.until(ExpectedConditions.visibilityOf(table));
+        for (int i = 0; i < emails.size(); i++) {
+            System.out.println(emails.get(i).getText());
+
+        }
     }
 }
---
+++ src/study/techno/implicitAndExplicitWait/LoaderDemo.java
@@ -0,0 +1,32 @@
+package study.techno.implicitAndExplicitWait;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utility.BaseClass;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class LoaderDemo extends BaseClass {
+    
+    public static void main(String[] args) {
+
+        WebDriverWait wait=new WebDriverWait(driver,60);
+
+        driver.get("http://demo.automationtesting.in/Loader.html");
+
+        driver.findElement(By.id("loader")).click();
+
+        WebElement pleaseWaitBox=driver.findElement(By.xpath("//div[@class='blockUI blockMsg blockPage']"));
+
+        //wait.until(ExpectedConditions.attributeToBe(By.id("myModal"),"style","display: block;"));
+
+        wait.until(ExpectedConditions.invisibilityOf(pleaseWaitBox));
+
+        WebElement paragraf=driver.findElement(By.xpath("//div[@id='myModal']//p"));
+
+        System.out.println(paragraf.getText());
+
+
+    }
+}
